2912e3a5dc222e4300f42fda1ef259672123ed64
Ralf Gabriels <ralfg@hotmail.be>
2020-08-01 11:20:49 +0200

    Working version, move to module structure

--- cascade_config/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Cascading configuration from the CLI and config files."""
-
-__version__ = "0.1.0"
--- cascade_config/cascade_config.py
+++
@@ -1,9 +0,0 @@
-"""Cascading configuration from the CLI and config files."""
-
-import collections.abc
-
-class Config(collections.abc.Mapping):
-    """Cascading configuration."""
-
-    def __init__(self) -> None:
-        """Cascading configuration"."""
